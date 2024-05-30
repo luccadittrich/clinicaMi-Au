288bf4a9f68dbf2015867ebe89460cd06b368716
Lucca Dittrich <ldslucca@hotmail.com>
2024-05-30 18:05:11 -0300

    ..

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -2,55 +2,8 @@ package main.java;
 
 public class Animal {
     private int id;
-    private String nome, especie, raca, genero, dataNascimento, cor, tutor;
-    private double peso;
+    private String nome, especie, raca, cor, CPFTutor;
+    private int peso, idade;
 
-    public Animal(int id, String nome, String especie, String raca, String genero, String dataNascimento, String cor,
-            String tutor, double peso) {
-        this.id = id;
-        this.nome = nome;
-        this.especie = especie;
-        this.raca = raca;
-        this.genero = genero;
-        this.dataNascimento = dataNascimento;
-        this.cor = cor;
-        this.tutor = tutor;
-        this.peso = peso;
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEspecie() {
-        return especie;
-    }
-
-    public String getRaca() {
-        return raca;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public String getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public String getCor() {
-        return cor;
-    }
-
-    public String getTutor() {
-        return tutor;
-    }
-
-    public double getPeso() {
-        return peso;
-    }
 }
