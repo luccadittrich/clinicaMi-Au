a4685fc463d036d22fec7c77c86307a8c682937b
Lucca Dittrich <ldslucca@hotmail.com>
2024-11-09 00:12:59 -0300

    correções

--- src/main/java/LoginFrame.java
+++ src/main/java/LoginFrame.java
@@ -1,4 +1,4 @@
-package main.java;
+// package main.java;
 import java.awt.event.*;
 import java.sql.*;
 import javax.swing.*;
--- src/main/java/cadastroPet.java
+++ src/main/java/cadastroPet.java
@@ -1,4 +1,3 @@
-package main.java;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
